4b51c2e2c5c2e0ecda0eba9446526783c5551665
João Gabriel Ferreira Macedo <20211204010018@ifrn.local>
2024-10-21 16:59:20 -0300

    Criando o controller de eventos

---
+++ src/main/java/ifrn/pi/eventos/controlles/EventosController.java
@@ -0,0 +1,17 @@
+package ifrn.pi.eventos.controlles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventosController {
+	
+	@RequestMapping("/eventos/forms")
+	public String form() {
+		return "formEvento";
+		
+	}
+	
+	
+
+}
