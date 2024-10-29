288a59e2b5a39311bd72c7b2a258ae675283947b
João Gabriel Ferreira Macedo <20211204010018@ifrn.local>
2024-10-29 09:50:18 -0300

    Enviando dados do formulario para aplicação

--- src/main/java/ifrn/pi/eventos/controlles/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controlles/EventosController.java
@@ -1,9 +1,6 @@
 package ifrn.pi.eventos.controlles;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pi.eventos.models.Evento;
@@ -17,13 +14,13 @@ public class EventosController {
 		
 		
 	}
-	@PostMapping("/eventos/submit")
+	@RequestMapping("/eventos/submit")
 	public String submit(Evento  evento) {
 		System.out.println("Evento salvo com os seguintes dados:");
-		System.out.println("Nome: " + evento.getNome());
-		System.out.println("Local: " + evento.getLocal());
-		System.out.println("Data: " + evento.getData());
-		System.out.println("Horário: " + evento.getHorario());
+		System.out.println(evento.getNome());
+		System.out.println(evento.getLocal());
+		System.out.println(evento.getData());
+		System.out.println(evento.getHorario());
 		return "executandosubmit";
 	}
 	
