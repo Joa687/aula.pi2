3a9523ed6beb23c6b7233ca9c3f848f2b1d56d3f
João Gabriel Ferreira Macedo <20211204010018@ifrn.local>
2024-11-18 16:41:47 -0300

    Cadastrando e listando os convidados do evento

--- src/main/java/ifrn/pi/eventos/controlles/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controlles/EventosController.java
@@ -1,14 +1,19 @@
 package ifrn.pi.eventos.controlles;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ifrn.pi.eventos.models.Convidado;
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.ConvidadoRepository;
 import ifrn.pi.eventos.repositories.eventoRepositorie;
 
 @Controller
@@ -17,6 +22,8 @@ public class EventosController {
 	
 	@Autowired
 	private eventoRepositorie er;
+	@Autowired
+	private ConvidadoRepository cr;
 	
 	@GetMapping("/form")
 	public String form() {
@@ -43,6 +50,42 @@ public class EventosController {
 		return mv;
 	}
 	
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Evento> opt = er.findById(id);
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/eventos");
+			return md;
+		}
+		
+		md.setViewName("eventos/detalhe");
+		Evento evento = opt.get();
+		md.addObject("evento", evento);
+		
+		List<Convidado> convidado = cr.findByEvento(evento);
+		md.addObject("convidado", convidado);
+		
+		return md;
+	}
 	
+	@PostMapping("/{idEvento}")
+	public String salvarConvidado(@PathVariable Long idEvento, Convidado convidado) {
+		
+		System.out.println("Id do evento: " + idEvento);
+		System.out.println(convidado);
+		
+		Optional<Evento> opt = er.findById(idEvento);
+		if(opt.isEmpty()) {
+			return "redirect:/eventos";
+		}
+		
+		Evento evento = opt.get();
+		convidado.setEvento(evento);
+		
+		cr.save(convidado);
+		
+		return "redirect:/eventos/{idEvento}";
+	}
 
 }
